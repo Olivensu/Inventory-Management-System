fe80d62a4444cb9f799ce8cdbac2b737762bcde3
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-03-24 16:38:53 +0600

    password encrypted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel')
-const bcrypt = require('bcryptjs')
+
 
 const registerUser= asyncHandler( async (req,res)=>{
     const {name, email,password} = req.body
@@ -23,15 +23,12 @@ const registerUser= asyncHandler( async (req,res)=>{
         throw new Error("Email has already been registered")
     }
 
-    //encrypt password before saving to DB
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
 
     // Create new user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword,
+        password,
     })
 
     if(user){
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -40,6 +41,18 @@ const userSchema = mongoose.Schema({
     timestamps: true,
 }
 )
+
+ //encrypt password before saving to DB
+ userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword
+ })
 
 const User = mongoose.model("User", userSchema)
 module.exports = User
