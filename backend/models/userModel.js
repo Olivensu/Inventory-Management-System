1da5f325e4c9afec35a331b264117187c3ce218e
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-03-24 16:26:33 +0600

    email password name set for backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add a email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: [6, "Password must be up to 6 Characters long"],
+        // maxLength: [23, "Password not more than 23 Characters long"]
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default : "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-1/319510614_635997608323100_7765184291606923409_n.jpg?stp=dst-jpg_p160x160&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHhnuVdkak6Rrc6FzaGDYV-i_Dlot9HGFWL8OWi30cYVeC1U7nDuEA1xqxp_8kcXaf5yxyKVI1L2cRWG6TNS1jZ&_nc_ohc=Y4qxW3fKTzYAX-akbrw&_nc_ht=scontent.fdac8-1.fna&oh=00_AfCH94ZmZVBFHDDwtN9nbrPREEa9WyCw5VFQU1nyJM-WjA&oe=6421AC1C"
+    },
+    phone: {
+        type: String,
+        default : "+880"
+    },
+    bio: {
+        type: String,
+        maxLength: [250, "Password not more than 250 Characters long"],
+        default : "bio"
+    },
+},
+{
+    timestamps: true,
+}
+)
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,30 @@
  const mongoose = require('mongoose');
  const bodyParser = require('body-parser');
  const cors = require('cors');
+ const userRoute = require('./routes/userRoute');
+ const errorHandler = require('./middleWare/errorMiddleWare');
 
  const app = express(); 
 
+ //middleware
+ app.use(express.json());
+ app.use(express.urlencoded({extended: false}));
+ app.use(bodyParser.json());
+
+ //Routes Middleware
+
+ app.use("/api/users", userRoute);
+
+ //Routes
+ app.get("/", (req, res)=>{
+    res.send("Home Page");
+
+ })
+
  const PORT = process.env.PORT || 5000;
 
+ //error Middleware
+ app.use(errorHandler);
  //connect to DB and start server'
 
  mongoose.connect(process.env.MONGO_URI)
